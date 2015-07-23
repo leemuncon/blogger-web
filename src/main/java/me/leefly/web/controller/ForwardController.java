92130cb208794522535fec22e7ae844e5c226a0e
LeeFly <leemuncon@163.com>
2015-07-23 18:36:39 +0800

    add

--- src/main/java/me/leefly/web/controller/ForwardController.java
+++ src/main/java/me/leefly/web/controller/ForwardController.java
@@ -1,7 +1,23 @@
 package me.leefly.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by LiFly on 2015/7/23.
  */
+@Controller
 public class ForwardController {
+
+    private static Logger logger = LoggerFactory.getLogger(ForwardController.class);
+
+    @RequestMapping(value = "home", method = RequestMethod.GET)
+    public String home(){
+        logger.info("ForwardController");
+        return "home";
+    }
+
 }
--- src/main/java/me/leefly/web/init/InitListener.java
+++ src/main/java/me/leefly/web/init/InitListener.java
@@ -1,19 +1,28 @@
 package me.leefly.web.init;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by LiFly on 2015/7/22.
  */
+@Component
 public class InitListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static Logger logger = LoggerFactory.getLogger(InitListener.class);
+
 
     public void onApplicationEvent(ContextRefreshedEvent event) {
         ApplicationContext context = event.getApplicationContext();
+        logger.info("The application {} was started", context);
         String[] names = context.getBeanDefinitionNames();
         for (String name : names) {
-            context.getBean(name);
+            Object bean = context.getBean(name);
+            logger.info("Initialized bean named {} and bean is {}", name, bean);
         }
     }
 
